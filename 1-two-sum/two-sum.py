a80554bc4de451872a35655106444a36481cfd61
Shital Jaykar <sheetaljaykar@gmail.com>
2026-07-21 21:37:44 +0530

    Time: 60 ms (35.38%) | Memory: 27.3 MB (6.98%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -9,3 +9,14 @@ class Solution:
                 return [num_to_index[complement], i]
             
             num_to_index[num] = i
+            class Solution:
+                def twoSum(self, nums: list[int], target: int) -> list[int]:
+                  seen = {}  # maps value -> index
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
+            
+        return []
